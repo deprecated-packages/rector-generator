59eb39890f55ae95871f72658e28b12d534573cc
Tomas Votruba <tomas.vot@gmail.com>
2021-07-14 23:54:47 +0200

    misc

--- ecs.php
+++ ecs.php
@@ -10,12 +10,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::PSR_12);
     $containerConfigurator->import(SetList::SYMPLIFY);
     $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import( SetList::CLEAN_CODE);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
     ]);
 
     $parameters->set(Option::SKIP, [
--- rector.php
+++ rector.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::PHP_80);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
 };
