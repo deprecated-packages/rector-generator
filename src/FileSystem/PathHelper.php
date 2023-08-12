31b9ca58ddf665566884fc88e71c0eb5e9d331d2
Tomas Votruba <tomas.vot@gmail.com>
2023-08-12 19:25:27 +0200

    require filesytem

--- src/FileSystem/PathHelper.php
+++ src/FileSystem/PathHelper.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Rector\RectorGenerator\FileSystem;
 
-use Symplify\SmartFileSystem\SmartFileSystem;
+use Symfony\Component\Filesystem\Filesystem;
 
 final class PathHelper
 {
     public static function getRelativePathFromDirector(string $filePath, string $directory): string
     {
-        $smartFileSystem = new SmartFileSystem();
-
+        $smartFileSystem = new Filesystem();
         $relativeFilePath = $smartFileSystem->makePathRelative($filePath, (string) realpath($directory));
 
         return rtrim($relativeFilePath, '/');
