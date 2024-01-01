fe886d1ae752832d0740e10352f951b704086206
Tomas Votruba <tomas.vot@gmail.com>
2024-01-01 14:47:48 -0100

    Remove Core namespace (#43)

--- templates/rules/__Package__/Rector/__Category__/__Name__.php
+++ templates/rules/__Package__/Rector/__Category__/__Name__.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Rector\__Package__\Rector\__Category__;
 
 use PhpParser\Node;
-use Rector\Core\Rector\AbstractRector;
+use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
