fbf6e766175a213630e7f3a3d4520c6777e2a63d
Tomas Votruba <tomas.vot@gmail.com>
2024-01-24 22:30:55 +0100

    tidy up

--- ecs.php
+++ ecs.php
@@ -3,19 +3,14 @@
 declare(strict_types=1);
 
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->sets([SetList::PSR_12, SetList::SYMPLIFY, SetList::COMMON, SetList::CLEAN_CODE, SetList::SYMPLIFY]);
-
-    $ecsConfig->paths([
+return ECSConfig::configure()
+    ->withPreparedSets(psr12: true, common: true, strict: true, symplify: true)
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/config',
         __DIR__ . '/tests',
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
         __DIR__ . '/templates/rector-recipe.php',
-    ]);
-
-    $ecsConfig->skip([__DIR__ . '/tests/RectorGenerator/Fixture', __DIR__ . '/tests/RectorGenerator/Source']);
-};
+    ])
+    ->withRootFiles()
+    ->withSkip([__DIR__ . '/tests/RectorGenerator/Fixture', __DIR__ . '/tests/RectorGenerator/Source']);
